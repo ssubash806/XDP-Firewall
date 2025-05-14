cf8ce16ecc4e379690dc08a77482972051407de8
trap-0x80 <ssubash806@gmail.com>
2025-05-14 21:30:50 +0530

    Added own implementation Base64 encode and decode

--- Test Suite/header/Base64.h
+++ Test Suite/header/Base64.h
@@ -1,11 +1,15 @@
-#include <iostream>
-#include <cstring>
-
-#include <boost/archive/iterators/base64_from_binary.hpp>
-#include <boost/archive/iterators/binary_from_base64.hpp>
-#include <boost/archive/iterators/transform_width.hpp>
-/* Implementation is derived from katran LB */
+#include <vector>
+#include <string>
 
 std::string base64Encode(std::string input_data);
 
 std::string base64Decode(std::string encoded);
+
+static const char base64_chars[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789+/";
+
+static inline bool is_base64(unsigned char c) {
+    return (isalnum(c) || c == '+' || c == '/');
+}
--- Test Suite/src/Base64.cpp
+++ Test Suite/src/Base64.cpp
@@ -3,39 +3,43 @@
 
 
 std::string base64Encode(std::string input_data) {
-    using namespace boost::archive::iterators;
-    using b64it = base64_from_binary<transform_width<const unsigned char*, 6, 8>>;
-    int output_size = (input_data.length() * 8 + 5) / 6;
-    std::string encoded(output_size, '*');
-    auto data = new char[input_data.length()];
-    if (data == nullptr) {
-      // return empty string. as we are using this in tests only that would just
-      // mean that we have failed a test. log line will show reason why.
-      std::cerr << "Memory allocation Failed for Base64 encode!" << std::endl;
-      return "";
+    std::string output;
+    int val = 0;
+    int valb = -6;
+    for (unsigned char c : input_data) {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 0) {
+            output.push_back(base64_chars[(val >> valb) & 0x3F]);
+            valb -= 6;
+        }
     }
-    std::memcpy(data, input_data.c_str(), input_data.length());
-    std::copy(b64it(data), b64it((char*)data + (input_data.length())), encoded.begin());
-    for (int i = 0; i < (3 - (input_data.length() % 3)) % 3; i++) {
-      encoded.push_back('=');
+    if (valb > -6) {
+        output.push_back(base64_chars[((val << 8) >> (valb + 8)) & 0x3F]);
     }
-    delete[] data;
-    return encoded;
+    while (output.size() % 4) {
+        output.push_back('=');
+    }
+    return output;
   }
   
   std::string base64Decode(std::string encoded) {
-    using namespace boost::archive::iterators;
-    using b64it =
-        transform_width<binary_from_base64<std::string::const_iterator>, 8, 6>;
-  
-    auto decoded =
-        std::string(b64it(std::begin(encoded)), b64it(std::end(encoded)));
-    int padded_chars = 0;
-    while (true) {
-      if (encoded[encoded.size() - 1] != '=') {
-        return decoded.substr(0, decoded.size() - padded_chars);
+      std::vector<int> T(256, -1);
+      for (int i = 0; i < 64; i++) {
+          T[base64_chars[i]] = i;
       }
-      encoded.pop_back();
-      padded_chars++;
-    }
+
+      std::string output;
+      int val = 0;
+      int valb = -8;
+      for (unsigned char c : encoded) {
+          if (T[c] == -1) break;
+          val = (val << 6) + T[c];
+          valb += 6;
+          if (valb >= 0) {
+              output.push_back(char((val >> valb) & 0xFF));
+              valb -= 8;
+          }
+      }
+      return output;
   }
